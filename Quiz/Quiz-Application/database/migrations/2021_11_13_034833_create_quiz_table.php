f9321fccaaac6b2b6dedc9df227493362ca664f5
shreyosi-roy <shreyosi@brandconfiance.com>
2021-11-15 17:15:42 +0530

    migration and model creation

---
+++ Quiz/Quiz-Application/database/migrations/2021_11_13_034833_create_quiz_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quiz', function (Blueprint $table) {
+            $table->id();
+            $table->string('Question');
+            $table->string('Option_one');
+            $table->string('Option_two');
+            $table->string('Option_three');
+            $table->string('Option_four');
+            $table->integer('correct_answer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quiz');
+    }
+}
